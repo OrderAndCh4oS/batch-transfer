175df7fe1cee0de74a424263dab215012b693ac4
order&chaos <sean@orderandchaoscreative.com>
2023-05-21 18:22:03 +0100

    Add address upload

--- src/lib/TransferTokens.svelte
+++ src/lib/TransferTokens.svelte
@@ -4,9 +4,12 @@
     import {user} from "../user";
     import {writable} from "svelte/store";
 
+    let addresses = writable([])
+
     const handleSend = async () => {
         await batchTransfer($user, $selected)
         selected.set([])
+        addresses.set(null)
     };
 
     let address = ''
@@ -14,28 +17,24 @@
         selected.update(selected => selected.map(s => ({...s, address})))
     }
 
-    let files;
+    let files, input;
 
     $: if (files) {
-        // Note that `files` is of type `FileList`, not an Array:
-        // https://developer.mozilla.org/en-US/docs/Web/API/FileList
-        console.log(files);
-
         for (const file of files) {
-            console.log(`${file.name}: ${file.size} bytes`);
             file.text().then(text => {
-                const addresses = text
+                const lines = text
                     .split('\n')
                     .map(line => line.trim())
                     .filter(line => line);
-                selected.update(selected => selected.map((s, i) => ({...s, address: i < addresses.length ? addresses[i] : ''})))
+                selected.update(selected => selected.map((s, i) => ({...s, address: i < lines.length ? lines[i] : ''})))
+                addresses.set(lines)
             })
         }
     }
 
     function shuffle(arr) {
         let randomizedArray = [];
-        let array = arr;
+        let array = [...arr];
         while(array.length !== 0) {
             let rIndex = Math.floor(array.length * Math.random());
             randomizedArray.push(array[rIndex]);
@@ -44,9 +43,14 @@
         return randomizedArray;
     }
 
-    const handleRandomiseAddresses = () => {
-        const addresses = shuffle($selected.map(s => s.address));
-        selected.update(selected => selected.map((s, i) => ({...s, address: i < addresses.length ? addresses[i] : ''})))
+    const handleRandomiseAddresses = (addresses) => () => {
+        const shuffledAddresses = addresses ? shuffle(addresses) : shuffle($selected.map(s => s.address));
+        selected.update(selected => selected.map((s, i) => ({...s, address: i < shuffledAddresses.length ? shuffledAddresses[i] : ''})))
+    }
+
+    const handleClearAddresses = () => {
+        input.value = ''
+        addresses.set(null)
     }
 </script>
 
@@ -66,9 +70,11 @@
                             id="file_input"
                             type="file"
                             bind:files
+                            bind:this={input}
                     />
                 </div>
-                <button on:click={handleRandomiseAddresses} class="px-2 py-2.5 border rounded border-woodsmoke hover:dark:border-swiss-coffee-200 hover:border-woodsmoke-100 dark:border-swiss-coffee-900">Randomise</button>
+                <button title="Clear the uploaded file and any unassigned addresses" on:click={handleClearAddresses} class="px-2 py-2.5 border rounded border-woodsmoke hover:dark:border-swiss-coffee-200 hover:border-woodsmoke-100 dark:border-swiss-coffee-900">Clear</button>
+                <button title="If an address file has been uploaded randomise will shuffle the list and reassign the tokens, otherwise it will shuffle the existing addresses in the form" on:click={handleRandomiseAddresses($addresses)} class="px-2 py-2.5 border rounded border-woodsmoke hover:dark:border-swiss-coffee-200 hover:border-woodsmoke-100 dark:border-swiss-coffee-900">Randomise</button>
             </div>
             <table class="w-full mb-4">
                 <thead>
